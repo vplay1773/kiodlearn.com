4d798013417dd359f5c9c3fdd3f50be757f06718
vplay1773 <68584392+vplay1773@users.noreply.github.com>
2020-10-06 13:37:43 -0700

    Anorax
    add

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+    <head> 
+        <title>kiodlearn.com</title>         
+        <meta charset="UTF-8"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <!-- CSS only -->         
+        <link href="style.css" rel="stylesheet"> 
+        <link href="css/product.css" rel="stylesheet"> 
+        <link href="css/bootstrap.min.css" rel="stylesheet"> 
+        <link href="css/bootstrap.css" rel="stylesheet"> 
+        <link href="css/hidden--visible.css" rel="stylesheet"> 
+        <link href="css/bootstrap-grid.css" rel="stylesheet"> 
+        <link href="css/bootstrap-reboot.css" rel="stylesheet"> 
+        <link href="css/bootstrap-reboot.min.css" rel="stylesheet"> 
+        <link href="css/bootstrap-grid.min.css" rel="stylesheet"> 
+        <link href="css/Offset.css" rel="stylesheet">
+        <link href="css/nav.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.css">
+        <!--JS, Popper.js, and jquery -->         
+        <script src="Scripts/jquery-1.10.2.min.js"></script>         
+        <script src="Scripts/bootstrap-rtl.js"></script>
+        <script src="Scripts/alert.js"></script>    
+        <script src="Scripts/button.js"></script>   
+    </head>     
+    <body>
+        <div class="cursor visible-lg visible-md visible-sm"></div>
+        <div class="cursor2"></div> 
+        <!--start nav-->        
+        <nav id="efec-scroll nav" class="navbar navbar-expand-md bg-dark navbar-dark fixed-top"> 
+            <buttom class="visible-xs" type="buttom" data-toggle="collapse" data-target="#navs"> 
+                <span class="navbar-toggler-icon"></span> 
+            </buttom>             
+            <a href="#">
+                <img src="images/Logo.png" id="Logo" class=" visible-xs" width="90px" alt="#">
+            </a>             
+            <div class="collapse navbar-collapse justify-content-center" id="navs"> 
+                <ul id="ulone" class="navbar-nav"> 
+                    <li class="nav-item item-li-1"> 
+                        <a href="#" class="nav-link">فروشگاه</a> 
+                    </li>                     
+                    <li class="nav-item item-li-1"> 
+                        <a href="#" class="nav-link item-a-2">آموزشات</a>
+                        <ul class="nav-ul-2 visible-xs" id="ul-2">
+                            <li style="list-style: none;" class="nav-li-3"><a href="#">آموزش کامپیوتر و اینترنت</a></li>
+                            <li style="list-style: none;" class="nav-li-3"><a href="#">اموزش سیستم عامل ها</a></li>
+                            <li style="list-style: none;" class="nav-li-3"><a href="#">اموزش فتوشاپ</a></li>
+                            <li style="list-style: none;" class="nav-li-3"><a href="#">skechware اموزش</a></li>
+                        </ul> 
+                        <ul class="item-ul-3 hidden-xs">
+                            <li class="item-li-4"><a href="#">آموزش کامپیوتر و اینترنت</a></li>
+                            <li class="item-li-4"><a href="#">اموزش سیستم عامل ها</a></li>
+                            <li class="item-li-4"><a href="#">اموزش فتوشاپ</a></li>
+                            <li class="item-li-4"><a href="#">skechware اموزش</a></li>
+                        </ul>
+                    </li>                     
+                    <li class="nav-item item-li-1"> 
+                        <a href="#" class="nav-link">افزونه ها</a> 
+                    </li>                     
+                </ul>                 
+                <img src="images/Logo.png" id="logo" class="hidden-xs" width="8%" alt="#"> 
+                <ul id="ultwn" class="navbar-nav"> 
+                    <li class="nav-item"> 
+                        <a href="#" class="nav-link item-li-1"> 
+                            &nbsp;&nbsp;&nbsp; TFC </a> 
+                    </li>                     
+                    <li class="nav-item item-li-1"> 
+                        <a href="#" class="nav-link item-a-2">طراحی سایت</a> 
+                        <ul class="nav-ul-2 visible-xs" id="ul-2">
+                            <li style="list-style: none;" class="nav-li-3"><a href="#">قالب وردپرس</a></li>
+                            <li style="list-style: none;" class="nav-li-3"><a href="#">html قالب </a></li>
+                        </ul>
+                        <ul class="item-ul-3 hidden-xs">
+                            <li class="item-li-4"><a href="#">قالب وردپرس</a></li>
+                            <li class="item-li-4"><a href="#">html قالب</a></li>
+                        </ul>
+                    </li>                     
+                    <li class="nav-item item-li-1"> 
+                        <a href="#" class="nav-link item-a-2">برنامه نویسی</a> 
+                        <ul class="nav-ul-2 visible-xs" id="ul-2">
+                            <li style="list-style: none;" class="nav-li-3"><a href="#">html ,css</a></li>
+                            <li style="list-style: none;" class="nav-li-3"><a href="#">java script ,type script ,jQuery</a></li>
+                            <li style="list-style: none;" class="nav-li-3"><a href="#">php ,sql</a></li>
+                            <li style="list-style: none;" class="nav-li-3"><a href="#">C# C++ C</a></li>
+                            <li style="list-style: none;" class="nav-li-3"><a href="#">Perl Ruby Python</a></li>
+                        </ul>
+                        <ul class="item-ul-3 hidden-xs">
+                            <li class="item-li-4"><a href="#" >html ,css</a></li>
+                            <li class="item-li-4"><a href="#">java script ,type script ,jQuery</a></li>
+                            <li class="item-li-4"><a href="#" >php ,sql</a></li>
+                            <li class="item-li-4"><a href="#" >C# C++ C</a></li>
+                            <li class="item-li-4"><a href="#" >Perl Ruby Python</a></li>
+                        </ul>
+                    </li>                     
+                </ul>                 
+            </div>             
+        </nav>         
+        <!--end nav-->        
+        <!--start header-->         
+        <header class="row text-center" id="header" style="height: auto;padding-top: 140px;padding-bottom: 50px;"> 
+            <div class="container content">
+                <h1 style="margin-bottom: 20px;" class="content">kiodlearn.io</h1>
+                <hr>
+                <h5 style="text-align-last: center;line-height: 50px;">
+                مدرسان با تجربه و کار ازموده در کیود لرن هستن و با قیمت رایگان در اختیار شما میزارن 
+                </h5>
+                <div class="contact-info">
+                    <div class="cart">
+                        <img src="images/icons8_timer_400px_2.png" class="icon">
+                        <div class="card-content">
+                            <h3>دقیقه آموزشی</h3>
+                            <span>صفر دقیقه</span>
+                        </div>
+                    </div>
+                    <div class="cart">
+                        <img src="images/icons8_books_400px.png" class="icon">
+                        <div class="card-content">
+                            <h3>دوره ها</h3>
+                            <span>صفر دوره</span>
+                        </div>
+                    </div>
+                    <div class="cart">
+                        <img src="images/icons8_teacher_400px_1.png" class="icon">
+                        <div class="card-content">
+                            <h3>مدرسان</h3>
+                            <span>صفر مدرس</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>   
+        <!--end header-->
+        <!--start content-->
+            <div class="row">
+                <div class="col-12" id="content-2" style="background-color: #f6f6f6;">
+                    
+                </div>
+            </div>
+        <!--end content-->         
+    </body>     
+    <script src="Scripts/jquery-1.10.2.min.js"></script>
+    <script>
+        var cursor = document.querySelector(".cursor");
+            var cursor2 = document.querySelector(".cursor2");
+            document.addEventListener("mousemove", function (e) {
+                cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+            });
+    </script>
+</html>
